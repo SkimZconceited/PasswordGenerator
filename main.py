d599d46979edc22ce87e94f6b128ccbd52b991ce
SkimZ <70768898+SkimZconceited@users.noreply.github.com>
2021-03-12 01:05:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
-letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-symbols = ["~","`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", ".", ",", ]
-upper = []
-lower = []
+import random
+import string
 
-for letter in letters:
-    upper.append(letter.upper())
-
-for letter in letters:
-    lower.append(letter.lower())
+def get_random_pass(length):
+    letters = string.ascii_letters
+    result_str = '' .join(random.choice(letters) for i in range(length)
     
-general = upper+lower+symbols+numbers
+    print(result_str)
 
-# for gen in general:
-print(len(general))
-
-a = 1
-int(a)
-
-b = 0
-int(b)
-
-print(*upper[b:a] + *lower[b:a])        #cant figure it out 
-a = a + 1
-b = b + 1
-print(*upper[b:a])
-a = a + 1
-b = b + 1
-print(*upper[b:a])
+get_random_pass(8)
